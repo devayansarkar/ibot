659a8e4dd486c7c9b2f47f907a7fe599e69ccc2a
Devayan Sarkar <devayansarkar@gmail.com>
2017-07-26 21:54:35 +0200

    fixing minor issues

--- ibot/views.py
+++ ibot/views.py
@@ -8,14 +8,12 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.utils.datastructures import MultiValueDictKeyError
 
-VERIFY_TOKEN = os.getenv('VERIFY_TOKEN')
-
 class BotController(generic.View):
     """Controlles and analyzes the request recieved from Facebook"""
     def get(self, *args, **kwargs):
         """Authenticates Facebook webhook"""
         try:
-            if self.request.GET['hub.verify_token'] == 'abc':
+            if self.request.GET['hub.verify_token'] == os.getenv('VERIFY_TOKEN'):
                 return HttpResponse(self.request.GET['hub.challenge'])
             else:
                 res = HttpResponse('Error, invalid token')
